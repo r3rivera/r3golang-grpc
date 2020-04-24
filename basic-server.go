077dd9db926da349995cede8653d058299762c85
Ryan Rivera <r.ryan.rivera@outlook.com>
2020-04-23 22:15:14 -0500

    Added server streaming rpc

--- basic-server.go
+++ basic-server.go
@@ -1,6 +1,9 @@
 package main
 
 import (
+	"context"
+	"time"
+
 	pb "github.com/r3rivera/r3app-protobuffer-repo/basic-test"
 
 	"log"
@@ -9,7 +12,58 @@ import (
 	"google.golang.org/grpc"
 )
 
-type server struct{}
+type serverHandler struct{}
+
+func (*serverHandler) HealthCheckStatus(ctx context.Context, rqst *pb.HealthCheckStatusRequest) (*pb.HealthCheckStatusResponse, error) {
+	log.Println("HealthCheckStatus function is called!")
+
+	status := pb.HealthCheckStatus{
+		AppName:       rqst.AppName,
+		AppReleaseVer: "1.3.45555",
+	}
+
+	resp := pb.HealthCheckStatusResponse{
+		HealthStatus: &status,
+	}
+	return &resp, nil
+}
+
+func (*serverHandler) Calculator(ctx context.Context, rqst *pb.CalculatorRequest) (*pb.CalculatorResponse, error) {
+	log.Println("Calculator function is called!", &rqst)
+
+	value := rqst.Payload.NumOne + rqst.Payload.NumTwo
+	log.Println("Sum of value is ", value)
+
+	resp := pb.CalculatorResponse{
+		Result: value,
+	}
+	return &resp, nil
+}
+
+//Server-Streamin API
+func (*serverHandler) NotificationMessage(rqst *pb.NotificationMessageRequest,
+	serverStream pb.NotificationMessageService_NotificationMessageServer) error {
+	log.Println("Server streaming is in progress!")
+
+	requester := rqst.Requester
+	log.Println("Caller of the stream is ", requester)
+
+	notify := pb.NotificationMessage{
+		Sender:  "Twitter",
+		Message: "Hello There!",
+	}
+
+	for i := 0; i < 25; i++ {
+		res := &pb.NotificationMessageResponse{
+			ResponsePayload: &notify,
+		}
+		serverStream.Send(res)
+		log.Println("Sleeping for 2 seconds... count is ", i)
+		time.Sleep(2 * time.Second)
+	}
+
+	return nil
+}
 
 func main() {
 	log.Println("Main Server...")
@@ -20,10 +74,13 @@ func main() {
 		panic(err)
 	}
 
-	s := grpc.NewServer()
-	pb.RegisterGreeterServiceServer(s, &server{})
+	server := grpc.NewServer()
 
-	if err := s.Serve(listener); err != nil {
+	pb.RegisterHealthCheckStatusServiceServer(server, &serverHandler{})
+	pb.RegisterCalculatorServiceServer(server, &serverHandler{})
+	pb.RegisterNotificationMessageServiceServer(server, &serverHandler{})
+
+	if err := server.Serve(listener); err != nil {
 		log.Fatalf("Failed to server: %v", err)
 	}
 	log.Println("Main server is up and running...")
--- go.mod
+++ go.mod
@@ -3,7 +3,7 @@ module r3golang-grpc
 go 1.14
 
 require (
-	github.com/r3rivera/r3app-protobuffer-repo v0.0.0-20200423191130-7ea097c21c0a
-	google.golang.org/grpc v1.29.1
+	github.com/r3rivera/r3app-protobuffer-repo v0.0.3 // indirect
+	google.golang.org/grpc v1.29.1 // indirect
 	google.golang.org/protobuf v1.21.0 // indirect
 )
